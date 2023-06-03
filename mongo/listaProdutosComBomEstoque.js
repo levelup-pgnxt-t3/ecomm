ceb5c6adc85f2d514a3578c38cbdeb0ecca59644
De Deus Morais Vitor Miguel Morais <n134364@gruposantander.com>
2023-06-02 22:25:11 -0300

    feat: Adicionado a função de venda de todos os livros

--- mongo/listaProdutosComBomEstoque.js
+++ mongo/listaProdutosComBomEstoque.js
@@ -1,5 +1,5 @@
 use("ecomm");
 
-const produtosComBomEstoque = db.products.find({ estoque: { $gte: 3 } }, { nome: 1, estoque: 1});
+const produtosComBomEstoque = db.products.find({estoque: {$gte: 3 } }, {nome: 1, estoque: 1});
 
 console.log(produtosComBomEstoque);
