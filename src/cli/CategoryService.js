e19bf7eb5b93d475f97967244d149a9dd940b981
Gabriela Riedel <gabrielariedel1@gmail.com>
2023-06-16 16:32:23 -0300

    Node.js: Listar as Categorias

---
+++ src/cli/CategoryService.js
@@ -0,0 +1,20 @@
+class CategoryService {
+    static findCategories() {
+      return fetch('http://localhost:3000/categories')
+        .then((response) => {
+          return response.json()
+            .then((data) => {
+              return {
+                statusCode: response.status,
+                data: data
+              };
+            });
+        })
+        .catch((error) => {
+          throw error;
+        });
+    }
+  }
+  
+ export default CategoryService
+  
--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,6 +1,23 @@
-export default function processarComando() {
+import CategoryService from "./CategoryService.js";
+
+function processarComando() {
     const argsLinhaDeComando = process.argv
-    console.log(argsLinhaDeComando)
+    const comando = argsLinhaDeComando[2]
+    switch (comando) {
+        case '--listarCategorias':
+          CategoryService.findCategories()
+            .then((categorias) => {
+              console.log('Status Code:', categorias.statusCode);
+              console.log('Categorias:', categorias.data);
+            })
+            .catch((error) => {
+              console.error('Erro ao listar categorias:', error);
+            });
+          break;
+    
+        default:
+          console.log('Comando inválido.');
+      }
 }
 
 processarComando()
