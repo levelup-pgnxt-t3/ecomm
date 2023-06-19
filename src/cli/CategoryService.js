8f8d3ae12cdae0170d59d803c30c401e22b2dd32
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2023-06-19 20:07:16 -0300

    Configuração do ESLint e correções sintáticas

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -1,85 +1,100 @@
-import chalk from "chalk"
+import chalk from 'chalk';
 import fs from 'fs';
 
-export default class CategoryService{
-  static async findCategories(){
-    try{
-      const response = await fetch('http://localhost:3000/categories')
-      if(response.ok){
-        const listaDeCategorias = await response.json();
-        console.log('status:', chalk.yellow(response.status))
-        return listaDeCategorias}
-    }catch(error) {trataErro(error);}
-  }
-
-  static async findCategoriesById(id){
-    try{
-      const response = await fetch(`http://localhost:3000/categories/${id}`);
-      if(response.ok){
-        const listaFiltrada = await response.json();
-        console.log('status:', chalk.yellow(response.status));
-        return listaFiltrada;}
-    }catch(error){trataErro(error)}
-  }
-
-  static async createCategory(caminho){
-    try{
-      const lerArquivo = await fs.promises.readFile(caminho, 'utf-8');
-      const adicionaCategoria = await fetch('http://localhost:3000/categories',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: lerArquivo
-      })
-      .then(response => {
-        console.log('response status: ' + chalk.yellow(response.status))
-        return response.json()})
-      .catch(error => console.log('Erro em inserir categoria: ' + error))
-
-      return adicionaCategoria;
-    } catch(error){trataErro(error)}
-  }
-
-  static async updateCategory(id, caminhoUpgrade){
-    try{
-      const lerArquivoUpgrade = await fs.promises.readFile(caminhoUpgrade, 'utf-8');
-      const atualizaCategoria = await fetch(`http://localhost:3000/categories/${id}`,
-      {
-        method:'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-      },
-      body: lerArquivoUpgrade
-      })
-      .then(response => {
-        console.log('response status: ' + chalk.yellow(response.status))
-        return response.json()})
-      .catch(error => console.log('Erro em inserir categoria: ' + error))
-  
-      return atualizaCategoria;
-    } catch(error){trataErro(error)}
-  }
-
-  static async deleteCategory(id){
-    try{
-      const deletaCategoria = await fetch(`http://localhost:3000/categories/${id}`,
-      {
-        method:'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-      }})
-      .then(response => {
-        console.log('response status: ' + chalk.yellow(response.status))
-        return response.json()})
-      .catch(error => console.log('Erro em deletar categoria: ' + error))
-  
-      return deletaCategoria;
-    } catch(error){trataErro(error)}
-  }
+function trataErro(error) {
+  console.log(chalk.red('Erro ao tentar executar ação: ', error));
 }
 
-function trataErro(error){
-   console.log(chalk.red('Erro ao tentar executar ação:' + error))
+export default class CategoryService {
+  static async findCategories() {
+    try {
+      const response = await fetch('http://localhost:3000/categories');
+      if (response.ok) {
+        const listaDeCategorias = await response.json();
+        console.log('status:', chalk.yellow(response.status));
+        return listaDeCategorias;
+      }
+    } catch (error) { trataErro(error); }
+    return 'Erro durante execução';
+  }
+
+  static async findCategoriesById(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/categories/${id}`);
+      if (response.ok) {
+        const listaFiltrada = await response.json();
+        console.log('status:', chalk.yellow(response.status));
+        return listaFiltrada;
+      }
+    } catch (error) { trataErro(error); }
+    return 'Erro durante execução';
+  }
+
+  static async createCategory(caminho) {
+    try {
+      const lerArquivo = await fs.promises.readFile(caminho, 'utf-8');
+      const adicionaCategoria = await fetch(
+        'http://localhost:3000/categories',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: lerArquivo,
+        },
+      )
+        .then((response) => {
+          console.log('response status: ', chalk.yellow(response.status));
+          return response.json();
+        })
+        .catch((error) => console.log('Erro em inserir categoria: ', error));
+
+      return adicionaCategoria;
+    } catch (error) { trataErro(error); }
+    return 'Erro durante execução';
+  }
+
+  static async updateCategory(id, caminhoUpgrade) {
+    try {
+      const lerArquivoUpgrade = await fs.promises.readFile(caminhoUpgrade, 'utf-8');
+      const atualizaCategoria = await fetch(
+        `http://localhost:3000/categories/${id}`,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: lerArquivoUpgrade,
+        },
+      )
+        .then((response) => {
+          console.log('response status: ', chalk.yellow(response.status));
+          return response.json();
+        })
+        .catch((error) => console.log('Erro em inserir categoria: ', error));
+      return atualizaCategoria;
+    } catch (error) { trataErro(error); }
+    return 'Erro durante execução';
+  }
+
+  static async deleteCategory(id) {
+    try {
+      const deletaCategoria = await fetch(
+        `http://localhost:3000/categories/${id}`,
+        {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+        .then((response) => {
+          console.log('response status: ', chalk.yellow(response.status));
+          return response.json();
+        })
+        .catch((error) => console.log('Erro em deletar categoria: ', error));
+      return deletaCategoria;
+    } catch (error) { trataErro(error); }
+    return 'Erro durante execução';
+  }
 }
--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,31 +1,39 @@
-import CategoryService from "./CategoryService.js";
+import CategoryService from './CategoryService.js';
 
 const caminho = process.argv;
 
-async function processarComando(argumento){
-  const comando = argumento[2]
-  switch(comando){
-    case "--listarCategorias":
-      const result = await CategoryService.findCategories();
+async function processarComando(argumento) {
+  const comando = argumento[2];
+  let result;
+  let resultId;
+  let insereCategoria;
+  let atualizaCategoria;
+  let excluiCategoria;
+  switch (comando) {
+    case '--listarCategorias':
+      result = await CategoryService.findCategories();
       console.log(result);
-      break
-    case "--recuperarCategoriaPorId":
-      const resultId = await CategoryService.findCategoriesById(argumento[3]);
+      break;
+    case '--recuperarCategoriaPorId':
+      resultId = await CategoryService.findCategoriesById(argumento[3]);
       console.log(resultId);
-      break
-    case "--inserirCategoria":
-      const insereCategoria = await CategoryService.createCategory(argumento[3]);
-      console.log(insereCategoria)
-      break
-    case "--atualizarCategoria":
-      const atualizaCategoria = await CategoryService.updateCategory(argumento[3],argumento[4]);
-      console.log('response data:', atualizaCategoria)
-      break
-    case "--excluirCategoria":
-      const excluiCategoria = await CategoryService.deleteCategory(argumento[3])
-      console.log(excluiCategoria)
-      break
+      break;
+    case '--inserirCategoria':
+      insereCategoria = await CategoryService.createCategory(argumento[3]);
+      console.log(insereCategoria);
+      break;
+    case '--atualizarCategoria':
+      atualizaCategoria = await CategoryService.updateCategory(argumento[3], argumento[4]);
+      console.log('response data:', atualizaCategoria);
+      break;
+    case '--excluirCategoria':
+      excluiCategoria = await CategoryService.deleteCategory(argumento[3]);
+      console.log(excluiCategoria);
+      break;
+    default:
+      console.log('Comando inválido');
+      break;
   }
 }
 
-processarComando(caminho)
+processarComando(caminho);
