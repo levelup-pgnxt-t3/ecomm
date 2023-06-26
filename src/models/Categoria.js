3666d5908a6d5c8dcdd4334fb8d55e5656adfabe
Gabriela Riedel <gabrielariedel1@gmail.com>
2023-06-26 15:59:42 -0300

    teste endpoints post produtos

--- src/models/Categoria.js
+++ src/models/Categoria.js
@@ -3,16 +3,7 @@ import mongoose from "mongoose";
 const categoriaSchema = new mongoose.Schema(
     {
         id: {type: String},
-        nome: {
-            type: String,
-            required: true,
-            validate: {
-                validator: function (value) {
-                    return /^(?!\d)\w{3,}$/.test(value);
-                },
-                message: "O nome da categoria é inválido. Deve ter mais de 3 caracteres e não pode começar com números."
-            }
-        },
+        nome: { type: String, required: true, match: /^(?![0-9])[a-zA-Z0-9]{3,}$/ },
         status: {type: String, required: true}
     },
     {
--- src/models/Produto.js
+++ src/models/Produto.js
@@ -2,46 +2,14 @@ import mongoose from "mongoose";
 
 const produtoSchema = new mongoose.Schema(
   {
-    nome: {
-      type: String,
-      required: true,
-      validate: {
-        validator: function (value) {
-          return /^(?!\\d)\\w{3,}$/.test(value);
-        },
-        message:
-          "O nome do produto é inválido. Deve ter mais de 3 caracteres e não pode começar com números.",
-      },
-    },
-    slug: {
-      type: String,
-      required: true,
-      validate: {
-        validator: function (value) {
-          return /^[a-zA-Z0-9-]+$/.test(value);
-        },
-        message:
-          "A slug do produto é inválida. Deve conter apenas letras maiúsculas ou minúsculas, números e hífens.",
-      },
-    },
-    precoUnitario: {
-      type: Number,
-      required: true,
-      min: 0.01,
-    },
-    quantidadeEstoque: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 10000,
-    },
-    categoriaId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "categorias",
-      required: true,
-    },
+    nome: { type: String, required: true, match: /^(?![0-9])\s*\S.{2,}$/ },
+    descricao: { type: String },
+    slug: { type: String, match: /^[a-zA-Z0-9-]+$/ },
+    preco: { type: Number, validate: (value) => value > 0 },
+    estoque: { type: Number, validate: (value) => (value > 0) && (value < 10000) },
+    categoria: { type: mongoose.Schema.Types.ObjectId, ref: 'categories', required: true, validate: (value) => mongoose.Types.ObjectId.isValid(value) }
   },
-  {
+    {
     versionKey: false,
   }
 );
